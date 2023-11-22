fb8634eaf2b720d207a795624e1ca97a0671e2b3
Marek <gawel.marek@xl.wp.pl>
2023-11-22 20:13:33 +0100

    zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// zmienne
+var name = "Ewa";
+var sex = "female";
+var age = 33;
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == "female" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (sex != "female" && age < 18)
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
